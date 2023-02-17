1daaf63df891d031b7e12cefc96cfb31cafaf1ed
d.klein@shopware.com <d.klein@shopware.com>
2023-02-17 08:52:15 +0100

    Modified 2 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjEwNjA0Mw/spectral.js?branch=quick-starter&token=260369f1-b86f-419b-afd4-8c43dd71e5d4";
+export default { extends: ruleset };
